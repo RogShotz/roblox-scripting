cb8f725c42e12d93e3ca36e4bf11970183aa3f5c
Luke Rowe <RogShotz@users.noreply.github.com>
2022-07-02 22:04:30 -0700

    Update roblox_rejoiner.py

--- roblox_rejoiner.py
+++ roblox_rejoiner.py
@@ -7,6 +7,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
+def kill_by_process_name_shell(name):
+    os.system("taskkill /f /im " + name)
+
+
 # xsrf Generator
 def getXsrf():
     authURL = "https://auth.roblox.com/v2/logout"
@@ -16,22 +20,33 @@ def getXsrf():
     return xsrfRequest
 
 
-ticketReq = requests.post("https://auth.roblox.com/v1/authentication-ticket/",
-                          headers={"x-csrf-token": getXsrf().headers['x-csrf-token'],
-                                   "referer": "https://www.roblox.com/home"},
-                          cookies={'.ROBLOSECURITY': os.getenv("ROBLOSEC")})
+def getAutTicket():
+    ticketReq = requests.post("https://auth.roblox.com/v1/authentication-ticket/",
+                              headers={"x-csrf-token": getXsrf().headers['x-csrf-token'],
+                                       "referer": "https://www.roblox.com/home"},
+                              cookies={'.ROBLOSECURITY': os.getenv("ROBLOSEC")})
+    return ticketReq.headers["rbx-authentication-ticket"]
 
-rbxAuth = ticketReq.headers["rbx-authentication-ticket"]
-rbxExecPATH = "C:\\Users\\rogsh\\AppData\\Local\\Roblox\\Versions\\version-cb81695a34b340de\\RobloxPlayerLauncher.exe"
+
+# Computer\HKEY_CLASSES_ROOT\roblox-player\shell\open\command
+rbxExecPATH = "C:\\Users\\rogsh\\Downloads\\synapse-launcher-11-17-21\\bin\\9QqpqI5vxy28.exe"
 rbxLaunches = "roblox-player:1+launchmode:play+"
-rbxGameInfo = f"gameinfo:{rbxAuth}"
+rbxGameInfo = f"gameinfo:{getAutTicket()}"
 rbxLaunchTime = "launchtime:1656807211123"
 rbxPlaceLauncherUrl = "placelauncherurl:https%3A%2F%2Fassetgame.roblox.com%2Fgame%2FPlaceLauncher.ashx%3Frequest%3DRequestGame%26browserTrackerId%3D129526326804%26placeId%3D9551640993%26isPlayTogetherGame%3Dfalse"
 rbxBrowserTrackerId = "browsertrackerid:129526326804"
 rbxLocales = "robloxLocale:en_us+gameLocale:en_us+channel:"
 
+
+os.system(
+        f'cmd /c "C:\\Users\\rogsh\\Downloads\\synapse-launcher-11-17-21\\Synapse Launcher.exe"')
+time.sleep(7)
 while True:
     os.system(
-        f'cmd /c "{rbxExecPATH} {rbxLaunches}+{rbxGameInfo}+{rbxLaunchTime}+{rbxPlaceLauncherUrl}+{rbxBrowserTrackerId}+{rbxLocales}"')
+       f'cmd /c "{rbxExecPATH} {rbxLaunches}+{rbxGameInfo}+{rbxLaunchTime}+{rbxPlaceLauncherUrl}+{rbxBrowserTrackerId}+{rbxLocales}"')
     print("Starting New Session")
-    time.sleep(1800)
+    time.sleep(20)
+    kill_by_process_name_shell("RobloxPlayerBeta.exe")
+    time.sleep(1)
+    rbxGameInfo = f"gameinfo:{getAutTicket()}"
+    time.sleep(5)
